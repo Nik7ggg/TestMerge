ad6d1bd220e25b27b2b628357e69c5426521f77d
Nik7ggg <47344019+Nik7ggg@users.noreply.github.com>
2019-04-18 14:58:30 +0700

    test

--- TestMerge/TestMerge.cpp
+++ TestMerge/TestMerge.cpp
@@ -1,11 +1,13 @@
 // TestMerge.cpp: определяет точку входа для консольного приложения.
 //
-
+#include "iostream"
 #include "stdafx.h"
 
-
+using namespace std;
 int main()
 {
+	cout << "Hello world";
+	cout << "test of merge";
     return 0;
 }
 
